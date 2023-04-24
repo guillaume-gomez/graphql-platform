90c37af4ce66150454ebd7d4c120d665ab0956bf
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-04-24 17:58:14 +0200

    add category entity

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -44,6 +44,12 @@ class Post
      */
     private $updatedAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -105,6 +111,18 @@ class Post
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
